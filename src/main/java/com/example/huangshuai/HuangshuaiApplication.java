62241646b36efb492d0822ecc349aa4b7d5e23f0
huangshuai <380962913@qq.com>
2019-03-04 16:05:50 +0800

    第一版代码提交

---
+++ src/main/java/com/example/huangshuai/HuangshuaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.huangshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuangshuaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangshuaiApplication.class, args);
+    }
+
+}
